086097f0911e0fb7bf38d8156e04a38db4a2fb68
LeperlierO <leperlierolivier3@gmail.com>
2024-01-22 16:09:28 +0100

    part 7 - auth guard and post create

---
+++ src/app/services/cat.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Cat } from '../models/Cat';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatService {
+
+  private catPath = environment.apiUrl + 'cats';
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+  create(data: any): Observable<Cat>{
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${this.authService.getToken()}`)
+
+    return this.http.post<Cat>(this.catPath, data, {headers});
+  }
+}
